905823b79016009d2c75e72fad011b778d2dd276
Lewis Raybould <124367113+LewisForks@users.noreply.github.com>
2024-06-01 20:27:21 +0100

    Create dashboard

--- src/Pages/dashboard/dashboard.js
+++ src/Pages/dashboard/dashboard.js
@@ -1,243 +1,296 @@
 import React from "react";
-import '../../App.css';
+import "../../App.css";
+import "./dashboard.css";
 
 import { Header } from "../../components/Layout/Header";
 
 const Dashboard = () => {
-    return (
-        <body>
+  return (
+    <div>
+      <div className="container">
+        <aside className="left-section">
+          <div className="logo">
+            <button className="menu-btn" id="menu-close">
+              <i className="bx bx-log-out-circle"></i>
+            </button>
+            {/* <img alt="logo" src="" /> */}
+            <a href="/dashboard">ImproveHub</a>
+          </div>
 
-    <div class="container">
-
-        <aside class="left-section">
-            <div class="logo">
-                <button class="menu-btn" id="menu-close">
-                    <i class='bx bx-log-out-circle'></i>
-                </button>
-             <img alt="logo" src=""/>
-                <a href="/dashboard">CollarCode</a>
+          <div className="sidebar">
+            <div className="item" id="active">
+              <i className="bx bx-home-alt-2"></i>
+              <a href="#">Overview</a>
             </div>
-
-            <div class="sidebar">
-                <div class="item" id="active">
-                    <i class='bx bx-home-alt-2'></i>
-                    <a href="#">Overview</a>
-                </div>
-                <div class="item">
-                    <i class='bx bx-grid-alt'></i>
-                    <a href="">Pets</a>
-                </div>
-                <div class="item">
-                    <i class='bx bx-folder'></i>
-                    <a href="#">Resources</a>
-                </div>
-                <div class="item">
-                    <i class='bx bx-message-square-dots'></i>
-                    <a href="#">Support</a>
-                </div>
-                <div class="item">
-                    <i class='bx bx-cog'></i>
-                    <a href="#">Settings</a>
-                </div>
+            <div className="item">
+              <i className="bx bx-grid-alt"></i>
+              <a href="">All Features</a>
             </div>
-
-            <div class="upgrade">
-                <h5>Got More Pets?</h5>
-                <div class="link">
-                    <a href="#">Upgrade to <b>PRO</b></a>
-                    <i class='bx bxs-chevron-right'></i>
-                </div>
+            <div className="item">
+              <i className="bx bx-folder"></i>
+              <a href="#">Resources</a>
             </div>
+            <div className="item">
+              <i className="bx bx-message-square-dots"></i>
+              <a href="#">Support</a>
+            </div>
+            <div className="item">
+              <i className="bx bx-cog"></i>
+              <a href="#">Settings</a>
+            </div>
+          </div>
 
+          <div className="upgrade">
+            <h5>Got More Pets?</h5>
+            <div className="link">
+              <a href="#">
+                Upgrade to <b>PRO</b>
+              </a>
+              <i className="bx bxs-chevron-right"></i>
+            </div>
+          </div>
         </aside>
 
-        <main id="swup" class="transition-fade">
-            <header>
-                <button class="menu-btn" id="menu-open">
-                    <i class='bx bx-menu'></i>
-                </button>
-                <h5>Hello <b>
-                        username
-                    </b>, welcome back!</h5>
-            </header>
+        <main>
+          <header>
+            <button className="menu-btn" id="menu-open">
+              <i className="bx bx-menu"></i>
+            </button>
+            <h5>
+              Hello <b>Lewis</b>, welcome back!
+            </h5>
+          </header>
 
-            <div class="separator">
-                <div class="info">
-                    <h3>My Pets</h3>
-                    <a class="btn" href="/account/create-pet-profile"><button>Create New</button></a>
-                    <a href="#">View All</a>
-                </div>
+          <div className="separator">
+            <div className="info">
+              <h3>My Goals</h3>
+              <a className="btn" href="/account/create-pet-profile">
+                <button>Create New</button>
+              </a>
+              <a href="#">View All</a>
             </div>
+          </div>
 
-            <div class="pets">
-                        <div class="item">
-                            <div class="progress">
-                                <div class="info">
-                                    <h5>
-                                        pet name
-                                    </h5>
-                                    <p>pet breed
-                                    </p>
-                                </div>
-                                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0"
-                                    aria-valuemax="100"></div>
-                            </div>
-                            <img class="qrcode" src="" alt=""/>
-                        </div>
-                                <div class="item" id="notfound">
-                                    <div class="progress">
-                                        <div class="info">
-                                            <h5>No Pets Found</h5>
-                                            <p><a href="/account/create-pet-profile">Create one</a> now!</p>
-                                        </div>
-                                    </div>
-                                    <i class='bx bx-x'></i>
-                                </div>
+          <div className="pets">
+            <div className="item">
+              <div className="progress">
+                <div className="info">
+                  <h5>Lose Fat</h5>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Duis dictum nisl augue, vitae accumsan odio imperdiet et.
+                  </p>
+                </div>
+                {/* <div
+                  className="progress-bar"
+                  role="progressbar"
+                  aria-valuenow="75"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                ></div> */}
+              </div>
+              <img className="qrcode" src="" alt="" />
             </div>
+            <div className="item" id="notfound">
+              <div className="progress">
+                <div className="info">
+                  <h5>No Goals Found</h5>
+                  <p>
+                    <a href="/account/create-pet-profile">Create one</a> now!
+                  </p>
+                </div>
+              </div>
+              <i className="bx bx-x"></i>
+            </div>
+          </div>
 
-            <div class="separator">
-                <div class="info">
-                    <h3>Emergency Contacts</h3>
-                    <a href="#">View All</a>
-                </div>
+          <div className="separator">
+            <div className="info">
+              <h3>Friends</h3>
+              <a href="#">View All</a>
             </div>
+          </div>
 
-            <div class="contacts">
-                <div class="item">
-                    <div class="left">
-                        <div class="icon">
-                            <img src="" alt="icon?"/>
-                        </div>
-                        <div class="details">
-                            <h5>Lewis Raybould</h5>
-                            <p>07925 847498</p>
-                            <p>lewis@raybould.co</p>
-                        </div>
-                    </div>
-                    <i class='bx bx-dots-vertical-rounded'></i>
+          <div className="contacts">
+            <div className="item">
+              <div className="left">
+                <div className="icon">
+                  <img src="" alt="icon?" />
                 </div>
-                <div class="item">
-                    <div class="left">
-                        <div class="icon">
-                            <img src="" alt="icon?"/>
-                        </div>
-                        <div class="details">
-                            <h5>Jack Penis</h5>
-                            <p>12345678</p>
-                            <p>poopoo@emails.com</p>
-                        </div>
-                    </div>
-                    <i class='bx bx-dots-vertical-rounded'></i>
+                <div className="details">
+                  <h5>MobExe</h5>
+                  <p>56 Points</p>
+                  <p>Last Active: 43 minutes ago</p>
                 </div>
+              </div>
+              <i className="bx bx-dots-vertical-rounded"></i>
             </div>
+            <div className="item">
+              <div className="left">
+                <div className="icon">
+                  <img src="" alt="icon?" />
+                </div>
+                <div className="details">
+                  <h5>Jerry</h5>
+                  <p>45 Points</p>
+                  <p>Last Active: 20 minutes ago</p>
+                </div>
+              </div>
+              <i className="bx bx-dots-vertical-rounded"></i>
+            </div>
+          </div>
         </main>
 
-        <aside class="right-section">
-            <div class="top">
-                <i class='bx bx-bell'></i>
-                <div class="profile">
-                    <div class="left">
-                        <img src="assets/profile.jpg"/>
-                        <div class="user">
-                            <h5>
-                                username
-                            </h5>
-                            <a href="#">Basic Plan</a>
-                        </div>
-                    </div>
-                    <i class='bx bxs-chevron-right'></i>
+        <aside className="right-section">
+          <div className="top">
+            <i className="bx bx-bell"></i>
+            <div className="profile">
+              <div className="left">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/2048px-Default_pfp.svg.png" />
+                <div className="user">
+                  <h5>username</h5>
+                  <a href="#">Basic Plan</a>
                 </div>
+              </div>
+              <i className="bx bxs-chevron-right"></i>
             </div>
+          </div>
 
-            <div class="separator" id="first">
-                <h4>Account Details</h4>
-            </div>
+          <div className="separator" id="first">
+            <h4>Account Details</h4>
+          </div>
 
-            <div class="account-details-container">
-                <div class="error-message" id="nameError"></div>
-                <div class="error-message" id="emailError"></div>
-                <div class="error-message" id="passwordError"></div>
-                <div class="error-message" id="dateOfBirthError"></div>
-                <div class="account-details">
-                    <div class="left-details">
-                        <h2>Name:</h2>
-                        <h2>Email</h2>
-                        <h2>Password:</h2>
-                        <h2>Date Of Birth:</h2>
-                        <h2 id="confirmPasswordText" style="display:none;">Current Password:</h2>
-                    </div>
-                    <div class="right-details">
-                        <p>
-                            username
-                        </p>
-                        <p>
-                            user email
-                        </p>
-                        <p>•••••••••••••</p>
-                        <p>
-                            user DOB
-                        </p>
-                    </div>
-                    <div class="right-details-form" style="display:none;">
-                        <form method="POST" id="accountDetailsForm">
-                            <div class="input-container">
-                                <input type="text" id="name" name="name" value="<%= userData.name %>" required/>
-                            </div>
-                            <div class="input-container">
-                                <input type="email" id="email" name="email" value="<%= userData.email %>" required/>
-                            </div>
-                            <div class="input-container">
-                                <input type="password" id="password" name="password" placeholder="Enter New Password"
-                                    required/>
-                            </div>
-                            <div class="input-container">
-                                <input type="date" id="dateOfBirth" name="dateOfBirth" value="<%= userData.dob %>"
-                                    required/>
-                            </div>
-                            <div class="input-container">
-                                <input type="password" id="confirmPassword" name="confirmPassword"
-                                    placeholder="Confirm Current Password" required/>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="account-details-btns">
-                    <button id="editBtn">Edit</button>
-                    <button id="submitBtn" style="display:none;">Submit</button>
-                    <button id="cancelBtn" style="display:none;">Cancel</button>
-                </div>
+          <div className="account-details-container">
+            <div className="error-message" id="nameError"></div>
+            <div className="error-message" id="emailError"></div>
+            <div className="error-message" id="passwordError"></div>
+            <div className="error-message" id="dateOfBirthError"></div>
+            <div className="account-details">
+              <div className="left-details">
+                <h2>Name:</h2>
+                <h2>Email</h2>
+                <h2>Password:</h2>
+                <h2>Date Of Birth:</h2>
+                <h2 id="confirmPasswordText" style={{ display: "none" }}>
+                  Current Password:
+                </h2>
+              </div>
+              <div className="right-details">
+                <p>username</p>
+                <p>user email</p>
+                <p>•••••••••••••</p>
+                <p>user DOB</p>
+              </div>
+              <div className="right-details-form" style={{ display: "none" }}>
+                <form method="POST" id="accountDetailsForm">
+                  <div className="input-container">
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      value="<%= userData.name %>"
+                      required
+                    />
+                  </div>
+                  <div className="input-container">
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      value="<%= userData.email %>"
+                      required
+                    />
+                  </div>
+                  <div className="input-container">
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      placeholder="Enter New Password"
+                      required
+                    />
+                  </div>
+                  <div className="input-container">
+                    <input
+                      type="date"
+                      id="dateOfBirth"
+                      name="dateOfBirth"
+                      value="<%= userData.dob %>"
+                      required
+                    />
+                  </div>
+                  <div className="input-container">
+                    <input
+                      type="password"
+                      id="confirmPassword"
+                      name="confirmPassword"
+                      placeholder="Confirm Current Password"
+                      required
+                    />
+                  </div>
+                </form>
+              </div>
             </div>
+            <div className="account-details-btns">
+              <button id="editBtn">Edit</button>
+              <button id="submitBtn" style={{ display: "none" }}>
+                Submit
+              </button>
+              <button id="cancelBtn" style={{ display: "none" }}>
+                Cancel
+              </button>
+            </div>
+          </div>
 
-            <div class="separator">
-                <h4>Account Statistics</h4>
-            </div>
+          <div className="separator">
+            <h4>Account Statistics</h4>
+          </div>
 
-            <div class="stats">
-                <div class="item">
-                    <div class="top">
-                        <p>Current</p>
-                        <p>Account Plan</p>
-                    </div>
-                    <div class="bottom">
-                        <div class="line"></div>
-                        <h3>Basic</h3>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="top">
-                        <p>Pet QR</p>
-                        <p>Codes Created</p>
-                    </div>
-                    <div class="bottom">
-                        <div class="line"></div>
-                        <h3>0</h3>
-                    </div>
-                </div>
+          <div className="stats">
+            <div className="item">
+              <div className="top">
+                <p>Current</p>
+                <p>Account Plan</p>
+              </div>
+              <div className="bottom">
+                <div className="line"></div>
+                <h3>Basic</h3>
+              </div>
             </div>
+            <div className="item">
+              <div className="top">
+                <p>Workouts</p>
+                <p>Completed</p>
+              </div>
+              <div className="bottom">
+                <div className="line"></div>
+                <h3>0</h3>
+              </div>
+            </div>
+            <div className="item">
+              <div className="top">
+                <p>Books Read</p>
+              </div>
+              <div className="bottom">
+                <div className="line"></div>
+                <h3>0</h3>
+              </div>
+            </div>
+            <div className="item">
+              <div className="top">
+                <p>Habits Left</p>
+                <p>To Complete</p>
+              </div>
+              <div className="bottom">
+                <div className="line"></div>
+                <h3>0</h3>
+              </div>
+            </div>
+          </div>
         </aside>
+      </div>
     </div>
-</body>
-    );
-}
+  );
+};
 
 export default Dashboard;
