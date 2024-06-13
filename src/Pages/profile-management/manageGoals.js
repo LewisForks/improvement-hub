ab6a92b938ae5eecf02671db5496be8d86c28967
Lewis Raybould <124367113+LewisForks@users.noreply.github.com>
2024-06-13 21:58:37 +0100

    Wrap goals in new container for specificity

--- src/Pages/profile-management/manageGoals.js
+++ src/Pages/profile-management/manageGoals.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { ManageGoals } from "../../components/profile-management/manageGoals";
+import { Goals } from "../../components/dashboard/main/goals/goals";
 import "../account-management/Auth.css";
 import "../../App.css";
-
+import "./manageGoals.css";
 
 import { Sidebar } from "../../components/Layout/Sidebar/Sidebar";
-import '../../components/Layout/Sidebar/Sidebar.css'
 
 const ManageGoalsPage = () => {
   return (
     <div>
-      <div className="container">
-      <Sidebar />
-      <ManageGoals />
+      <div className="goals-container">
+        <Sidebar />
+        <div className="manage-goals">
+          <Goals />
+        </div>
       </div>
     </div>
   );
