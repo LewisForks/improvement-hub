fa751c25b15fed9559d30ec6e4d8d7f023a8576a
Lewis Raybould <124367113+LewisForks@users.noreply.github.com>
2024-06-13 20:16:20 +0100

    Implement location checks for "active" ID

--- src/components/Layout/Sidebar/Sidebar.js
+++ src/components/Layout/Sidebar/Sidebar.js
@@ -1,48 +1,51 @@
-import './Sidebar.css';
+import "./Sidebar.css";
+import { useLocation } from "react-router-dom";
 
 export const Sidebar = () => {
-    return (
-        <aside className="left-section">
-          <div className="logo">
-            <button className="menu-btn" id="menu-close">
-              <i className="bx bx-log-out-circle"></i>
-            </button>
-            {/* <img alt="logo" src="" /> */}
-            <a href="/dashboard">ImproveHub</a>
-          </div>
+  const location = useLocation();
 
-          <div className="sidebar">
-            <div className="item" id="active">
-              <i className="bx bx-home-alt-2"></i>
-              <a href="#">Overview</a>
-            </div>
-            <div className="item">
-              <i className="bx bx-grid-alt"></i>
-              <a href="">All Features</a>
-            </div>
-            <div className="item">
-              <i className="bx bx-folder"></i>
-              <a href="#">Resources</a>
-            </div>
-            <div className="item">
-              <i className="bx bx-message-square-dots"></i>
-              <a href="#">Support</a>
-            </div>
-            <div className="item">
-              <i className="bx bx-cog"></i>
-              <a href="#">Settings</a>
-            </div>
-          </div>
+  return (
+    <aside className="left-section">
+      <div className="logo">
+        <button className="menu-btn" id="menu-close">
+          <i className="bx bx-log-out-circle"></i>
+        </button>
+        {/* <img alt="logo" src="" /> */}
+        <a href="/dashboard">ImproveHub</a>
+      </div>
 
-          <div className="upgrade">
-            <h5>Got More Pets?</h5>
-            <div className="link">
-              <a href="#">
-                Upgrade to <b>PRO</b>
-              </a>
-              <i className="bx bxs-chevron-right"></i>
-            </div>
-          </div>
-        </aside>
-    );
+      <div className="sidebar">
+        <div className="item" id={location.pathname === "/dashboard" ? "active" : ""}>
+          <i className="bx bx-home-alt-2"></i>
+          <a href="#">Overview</a>
+        </div>
+        <div className="item" id={location.pathname === "/dashboard/features" ? "active" : ""}>
+          <i className="bx bx-grid-alt"></i>
+          <a href="">All Features</a>
+        </div>
+        <div className="item" id={location.pathname === "/dashboard/resources" ? "active" : ""}>
+          <i className="bx bx-folder"></i>
+          <a href="#">Resources</a>
+        </div>
+        <div className="item" id={location.pathname === "/dashboard/support" ? "active" : ""}>
+          <i className="bx bx-message-square-dots"></i>
+          <a href="#">Support</a>
+        </div>
+        <div className="item" id={location.pathname === "/dashboard/settings" ? "active" : ""}>
+          <i className="bx bx-cog"></i>
+          <a href="#">Settings</a>
+        </div>
+      </div>
+
+      <div className="upgrade">
+        <h5>Want all features?</h5>
+        <div className="link">
+          <a href="#">
+            Upgrade to <b>PRO</b>
+          </a>
+          <i className="bx bxs-chevron-right"></i>
+        </div>
+      </div>
+    </aside>
+  );
 };
