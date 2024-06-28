48eff91f0472164d879ed0a190062db42c821c50
Lewis Raybould <124367113+LewisForks@users.noreply.github.com>
2024-06-28 11:01:37 +0100

    Add goalsComplete logic to account stats

--- src/components/dashboard/allFeatures/accountStatistics/accountStatistics.js
+++ src/components/dashboard/allFeatures/accountStatistics/accountStatistics.js
@@ -1,6 +1,48 @@
 import "./accountStatistics.css";
+import { useEffect, useState } from "react";
+import { db } from "../../../../config/firebase";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { auth } from "../../../../config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 export const AccountStatistics = () => {
+  const [goalsComplete, setGoalsComplete] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoading(true);
+        const q = query(
+          collection(db, "goals"),
+          where("userId", "==", user.uid),
+          where("completed", "==", true)
+        );
+        const unsubscribeSnapshot = onSnapshot(
+          q,
+          (snapshot) => {
+            setGoalsComplete(snapshot.size);
+            setLoading(false);
+          },
+          (error) => {
+            console.error("Error fetching goals: ", error);
+            setLoading(false);
+          }
+        );
+
+        // Clean up the onSnapshot listener
+        return () => unsubscribeSnapshot();
+      }
+    });
+
+    // Clean up the onAuthStateChanged listener
+    return () => unsubscribe();
+  }, [auth, db]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <div className="separator">
@@ -20,7 +62,6 @@ export const AccountStatistics = () => {
         <div className="item">
           <div className="top">
             <p>Workouts Complete</p>
-
           </div>
           <div className="bottom">
             <div className="line"></div>
@@ -51,7 +92,7 @@ export const AccountStatistics = () => {
           </div>
           <div className="bottom">
             <div className="line"></div>
-            <h3>0</h3>
+            <h3>{goalsComplete}</h3>
           </div>
         </div>
         <div className="item">
